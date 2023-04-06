e6ffd7a4e16b322ee21666d6aadca3b9b772e9f8
Loris Sigrist <sigrist@hasch.io>
2023-04-06 17:43:17 +0200

    Minify HTML during build

---
+++ src/hooks.server.ts
@@ -0,0 +1,35 @@
+import { minify, type Options } from 'html-minifier';
+import { building } from '$app/environment';
+
+const minification_options: Options = {
+  collapseBooleanAttributes: true,
+  collapseWhitespace: true,
+  conservativeCollapse: true,
+  decodeEntities: true,
+  html5: true,
+  ignoreCustomComments: [/^#/],
+  minifyCSS: true,
+  minifyJS: true,
+  removeAttributeQuotes: true,
+  removeComments: false, // some hydration code needs comments, so leave them in
+  removeOptionalTags: true,
+  removeRedundantAttributes: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  sortAttributes: true,
+  sortClassName: true
+};
+
+/** @type {import('@sveltejs/kit').Handle} */
+export async function handle({ event, resolve }) {
+  let page = '';
+
+  return resolve(event, {
+    transformPageChunk: ({ html, done }) => {
+      page += html;
+      if (done) {
+        return building ? minify(page, minification_options) : page;
+      }
+    }
+  });
+}
