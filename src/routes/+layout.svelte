390b5b481d096f00a9875eb672c751093bec019d
Loris Sigrist <sigrist@hasch.io>
2023-04-06 18:58:58 +0200

    If there is no theme preference, go with the OS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,9 +12,21 @@
 	<script>
 		if ('localStorage' in window) {
 			const prefers_dark = localStorage.getItem('theme') === 'dark';
-
+			const prefers_light = localStorage.getItem('theme') === 'light';
+			const prefers_nothing = !prefers_dark && !prefers_light;
 			if (prefers_dark) {
 				document.documentElement.classList.add('dark');
+			} else if (prefers_light) {
+				document.documentElement.classList.remove('dark');
+			} else if (prefers_nothing) {
+				const media_query = window.matchMedia('(prefers-color-scheme: dark)');
+				if (media_query.matches) {
+					document.documentElement.classList.add('dark');
+					localStorage.setItem('theme', 'dark');
+				} else {
+					document.documentElement.classList.remove('dark');
+					localStorage.setItem('theme', 'light');
+				}
 			}
 		}
 	</script>
