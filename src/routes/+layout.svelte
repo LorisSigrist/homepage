46779ad22b3a2804a46d759983ef76c4e9cee721
Loris Sigrist <sigrist@hasch.io>
2023-04-06 17:37:44 +0200

    Moved the theme-initializer inline-script to the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,22 @@
 	import Header from '$lib/ui/Header.svelte';
 </script>
 
+<svelte:head>
+	<!-- 
+		This initializes the dark / light theme. 
+		It's supposed to be inlined in the html and run blockingly
+	-->
+	<script>
+		if ('localStorage' in window) {
+			const prefers_dark = localStorage.getItem('theme') === 'dark';
+
+			if (prefers_dark) {
+				document.documentElement.classList.add('dark');
+			}
+		}
+	</script>
+</svelte:head>
+
 <Metadata />
 
 <Header />
