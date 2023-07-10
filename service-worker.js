0fd7bb2e31aad2b9b7f255a73ed46ca94df70fc7
LorisSigrist <43482866+LorisSigrist@users.noreply.github.com>
2023-07-10 14:05:08 +0000

    Deploying to gh-pages from @ LorisSigrist/homepage@38652377a1aca86bc834f2275a7f6866bb355f4f 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,71 +1,76 @@
-const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), p = [
-  a + "/_app/immutable/entry/app.c09593a3.js",
-  a + "/_app/immutable/assets/0.f5c19244.css",
-  a + "/_app/immutable/nodes/0.cbf0a5cf.js",
-  a + "/_app/immutable/assets/avatar.4558b389.png",
-  a + "/_app/immutable/nodes/1.1bb6634b.js",
-  a + "/_app/immutable/assets/2.3d7ef7ec.css",
-  a + "/_app/immutable/nodes/2.4c5fa574.js",
-  a + "/_app/immutable/assets/3.8b9ba990.css",
-  a + "/_app/immutable/nodes/3.ad648cfe.js",
-  a + "/_app/immutable/nodes/4.7463e73a.js",
-  a + "/_app/immutable/nodes/5.7ab993d7.js",
-  a + "/_app/immutable/assets/for-await-of-compat.16fd3cc1.png",
-  a + "/_app/immutable/assets/raw-stream.af5d3091.gif",
-  a + "/_app/immutable/assets/text-stream.8cbdd213.gif",
-  a + "/_app/immutable/assets/parse-result-value-stream.dc55853e.gif",
-  a + "/_app/immutable/assets/final-stream.7ab47112.gif",
-  a + "/_app/immutable/assets/non-stream-load.67378d23.gif",
-  a + "/_app/immutable/assets/stream-load.a3f8ce89.gif",
-  a + "/_app/immutable/chunks/index.4190156f.js",
-  a + "/_app/immutable/chunks/index.c3b38f65.js",
-  a + "/_app/immutable/chunks/singletons.c73804aa.js",
-  a + "/_app/immutable/chunks/stores.e17b4953.js",
-  a + "/_app/immutable/chunks/theme.de549241.js",
-  a + "/_app/immutable/entry/start.e4570af4.js"
-], m = [
-  a + "/.nojekyll",
-  a + "/favicon.png"
+const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m = [
+  e + "/_app/immutable/entry/app.702e65e0.js",
+  e + "/_app/immutable/assets/0.7e8976f9.css",
+  e + "/_app/immutable/nodes/0.d4a6735c.js",
+  e + "/_app/immutable/assets/avatar.4558b389.png",
+  e + "/_app/immutable/nodes/1.e5b0b72f.js",
+  e + "/_app/immutable/assets/2.57052bc9.css",
+  e + "/_app/immutable/nodes/2.9e201422.js",
+  e + "/_app/immutable/assets/3.8b9ba990.css",
+  e + "/_app/immutable/nodes/3.3e6ba3eb.js",
+  e + "/_app/immutable/nodes/4.6fd3f7a4.js",
+  e + "/_app/immutable/nodes/5.ad278d36.js",
+  e + "/_app/immutable/nodes/6.d3f4949a.js",
+  e + "/_app/immutable/nodes/7.08f4601b.js",
+  e + "/_app/immutable/assets/for-await-of-compat.16fd3cc1.png",
+  e + "/_app/immutable/assets/raw-stream.af5d3091.gif",
+  e + "/_app/immutable/assets/text-stream.8cbdd213.gif",
+  e + "/_app/immutable/assets/parse-result-value-stream.dc55853e.gif",
+  e + "/_app/immutable/assets/final-stream.7ab47112.gif",
+  e + "/_app/immutable/assets/non-stream-load.67378d23.gif",
+  e + "/_app/immutable/assets/stream-load.a3f8ce89.gif",
+  e + "/_app/immutable/chunks/index.4190156f.js",
+  e + "/_app/immutable/chunks/index.c3b38f65.js",
+  e + "/_app/immutable/chunks/singletons.9fe5eb71.js",
+  e + "/_app/immutable/chunks/stores.19601eeb.js",
+  e + "/_app/immutable/chunks/theme.de549241.js",
+  e + "/_app/immutable/entry/start.0bf37e19.js"
+], l = [
+  e + "/.nojekyll",
+  e + "/favicon.png"
 ], o = [
-  a + "/",
-  a + "/__data.json",
-  a + "/mock-data-with-zocker",
-  a + "/the-better-way-to-load-data",
-  a + "/og/Loris Sigrist.png",
-  a + "/og/Never write Mock Data again, with Zocker.png",
-  a + "/og/The better way to load data.png"
-], l = "1688984203092", n = `cache-${l}`, c = [
-  ...p,
-  // the app itself
+  e + "/",
+  e + "/__data.json",
+  e + "/color-thief",
+  e + "/mock-data-with-zocker",
+  e + "/reliably-avoiding-theme-flashes",
+  e + "/the-better-way-to-load-data",
+  e + "/og/Loris Sigrist.png",
+  e + "/og/Never write Mock Data again, with Zocker.png",
+  e + "/og/Reliably Avoiding Theme flashes.png",
+  e + "/og/The better way to load data.png"
+], c = "1688997887804", n = `cache-${c}`, p = [
   ...m,
+  // the app itself
+  ...l,
   // everything in `static`
   ...o
   // prerendered pages
 ];
-self.addEventListener("install", (e) => {
+self.addEventListener("install", (a) => {
   async function s() {
-    await (await caches.open(n)).addAll(c);
+    await (await caches.open(n)).addAll(p);
   }
-  e.waitUntil(s());
+  a.waitUntil(s());
 });
-self.addEventListener("activate", (e) => {
+self.addEventListener("activate", (a) => {
   async function s() {
     for (const t of await caches.keys())
       t !== n && await caches.delete(t);
   }
-  e.waitUntil(s());
+  a.waitUntil(s());
 });
-self.addEventListener("fetch", (e) => {
-  const { request: s } = e, t = new URL(e.request.url);
+self.addEventListener("fetch", (a) => {
+  const { request: s } = a, t = new URL(a.request.url);
   if (s.method !== "GET") {
-    e.respondWith(fetch(s));
+    a.respondWith(fetch(s));
     return;
   }
-  if (!c.includes(t.pathname)) {
-    e.respondWith(fetch(s));
+  if (!p.includes(t.pathname)) {
+    a.respondWith(fetch(s));
     return;
   }
-  e.respondWith(
+  a.respondWith(
     caches.open(n).then((i) => i.match(t.pathname)).then((i) => i || (console.error("Cache miss for: " + t.pathname), fetch(s)))
   );
 });
