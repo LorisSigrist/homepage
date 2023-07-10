97fa911ca5ab91664a17d2d2d548d65bef6fd395
LorisSigrist <43482866+LorisSigrist@users.noreply.github.com>
2023-07-10 15:47:08 +0000

    Deploying to gh-pages from @ LorisSigrist/homepage@8dbb133bab6e57911ffda96aa566cffec8793520 🚀

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+self.addEventListener("activate", () => {
+  self.clients.matchAll({
+    type: "window"
+  }).then((t) => {
+    t.forEach((e) => {
+      e.navigate(e.url);
+    });
+  });
+});
