1c111557f894423f8bf7fba4e57990730501e625
Loris Sigrist <sigrist@hasch.io>
2023-07-10 17:34:00 +0200

    Removed scale-in animation on landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 
 <Topology />
 
-<div class="py-24 sm:py-32 scale-in">
+<div class="py-24 sm:py-32">
 	<div class="mx-auto max-w-7xl px-6 lg:px-8">
 		<div class="mx-auto max-w-2xl">
 			<h1 class="text-8xl font-black mb-8">Loris Sigrist</h1>
@@ -19,21 +19,3 @@
 		</div>
 	</div>
 </div>
-
-<style>
-	.scale-in {
-		animation: scale-in 1s ease-in-out;
-		animation-fill-mode: backwards;
-	}
-
-	@keyframes scale-in {
-		0% {
-			transform: scale(0.9);
-			opacity: 0;
-		}
-		100% {
-			transform: scale(1);
-			opacity: 1;
-		}
-	}
-</style>
