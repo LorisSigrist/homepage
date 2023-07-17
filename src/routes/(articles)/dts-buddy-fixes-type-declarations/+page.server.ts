b0dfd505ba831e9db37cc57743a65c38e3f8c662
Loris Sigrist <sigrist@hasch.io>
2023-07-17 09:00:08 +0200

    We do a little writing

---
+++ src/routes/(articles)/dts-buddy-fixes-type-declarations/+page.server.ts
@@ -0,0 +1,11 @@
+import type { ArticleMetadata } from "$lib/articles/types";
+
+export function load(): ArticleMetadata {
+    return {
+        title: "dts-buddy fixes type declarations",
+        description: "dts-buddy is a bundler for type-definitions. It helps sidestep the most common issues with type definitions.",
+        published: new Date(2023, 6, 16),
+        author: "Loris Sigrist",
+        draft: true,
+    }
+}
