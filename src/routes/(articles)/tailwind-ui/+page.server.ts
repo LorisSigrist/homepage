9460838b8f5ceb1a20e94772a8fbc07e58e0c521
Loris Sigrist <sigrist@hasch.io>
2023-07-31 09:42:13 +0200

    Finish + publish

--- src/routes/(articles)/tailwind-ui/+page.server.ts
+++ src/routes/(articles)/tailwind-ui/+page.server.ts
@@ -4,8 +4,7 @@ export function load(): ArticleMetadata {
     return {
         title: "Is Tailwind UI worth it?",
         description: `Tailwind UI is a collection of high-quality components and templates built by the creators of Tailwind CSS. It's 300$ price-tag seems quite steep considering all the free alternatives. Could it really be worth it? Here are my thoughts after a few months of use`,
-        published: new Date("2023-08-01"),
+        published: new Date("2023-07-31"),
         author: "Loris Sigrist",
-        draft: true,
     }
 }
