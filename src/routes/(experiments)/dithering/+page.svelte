429d60a786e2073639c67902cd52d901a78de4ed
Loris Sigrist <sigrist@hasch.io>
2023-08-05 19:24:01 +0200

    Better layout

--- src/routes/(experiments)/dithering/+page.svelte
+++ src/routes/(experiments)/dithering/+page.svelte
@@ -7,8 +7,8 @@
 	import panzoom, { type PanZoomOptions } from 'panzoom';
 
 	import { dithering } from './rendering/index';
-	import { onMount } from 'svelte';
 	import ImageInput from './ImageInput.svelte';
+	import Button from './Button.svelte';
 
 	let threshold = 0.33;
 	let noiseIntensity = 0.3;
@@ -16,9 +16,7 @@
 	let colorLight = '#ede6cc';
 	let colorDark = '#21263f';
 
-	let canvas : HTMLCanvasElement | null = null;
-
-	let css_image_rendering_pixelated = true;
+	let canvas: HTMLCanvasElement | null = null;
 
 	let width = 600;
 
@@ -29,10 +27,6 @@
 	let loaded_image: HTMLImageElement | null = null;
 	$: aspectRatio =
 		loaded_image?.width && loaded_image?.height ? loaded_image.width / loaded_image.height : 1;
-
-	onMount(() => {
-		css_image_rendering_pixelated = Number.isInteger(window.devicePixelRatio);
-	});
 
 	async function save() {
 		if (!canvas) return;
@@ -47,8 +41,8 @@
 		const link = document.createElement('a');
 		link.download = 'dithered.png';
 		const blob = await intermediate.convertToBlob({
-			"type": "image/png",
-			"quality": 1
+			type: 'image/png',
+			quality: 1
 		});
 
 		link.href = URL.createObjectURL(blob);
@@ -58,12 +52,11 @@
 		URL.revokeObjectURL(link.href);
 	}
 
-
-	function panzoomAction(element : HTMLElement, options?: PanZoomOptions)  {
+	function panzoomAction(element: HTMLElement, options?: PanZoomOptions) {
 		const pz = panzoom(element, options);
 		return {
 			destroy: () => pz.dispose()
-		}
+		};
 	}
 </script>
 
@@ -72,81 +65,81 @@
 </svelte:head>
 
 <main class="w-screen max-w-screen h-screen max-h-screen flex">
-	<section
-		class="bg-white px-4 w-full max-w-md py-8 border-r border-gray-100 overflow-y-auto grid gap-4 shadow-md"
+	<!--Sidebar-->
+	<aside
+		class="bg-white w-full max-w-md border-r border-gray-100 overflow-y-hidden h-full shadow-md flex flex-col"
 	>
-		<ImageInput bind:image={loaded_image} />
+		<section class="grid gap-4 flex-1 h-0 overflow-y-auto overflow-x-visible py-8 px-4">
+			<ImageInput bind:image={loaded_image} />
 
-		<Select
-			label="Dither Mode"
-			options={[
-				{
-					name: 'Bayer',
-					value: 'bayer'
-				},
-				{
-					name: 'Blue Noise',
-					value: 'blue_noise'
-				},
-				{
-					name: 'White Noise',
-					value: 'white_noise'
-				}
-			]}
-			bind:selected={mode}
-		/>
+			<Select
+				label="Dither Mode"
+				options={[
+					{
+						name: 'Bayer',
+						value: 'bayer'
+					},
+					{
+						name: 'Blue Noise',
+						value: 'blue_noise'
+					},
+					{
+						name: 'White Noise',
+						value: 'white_noise'
+					}
+				]}
+				bind:selected={mode}
+			/>
 
-		<Slider label="Threshold ({threshold})" min={0} max={1} step={0.01} bind:value={threshold} />
+			<Slider label="Threshold ({threshold})" min={0} max={1} step={0.01} bind:value={threshold} />
 
-		<Slider
-			label="Noise Intensity ({noiseIntensity})"
-			min={0}
-			max={1.5}
-			step={0.01}
-			bind:value={noiseIntensity}
-		/>
+			<Slider
+				label="Noise Intensity ({noiseIntensity})"
+				min={0}
+				max={1.5}
+				step={0.01}
+				bind:value={noiseIntensity}
+			/>
 
-		<div class="relative flex items-start">
-			<div class="flex h-6 items-center">
-				<input
-					type="checkbox"
-					bind:checked={monochrome}
-					class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-				/>
+			<div class="relative flex items-start">
+				<div class="flex h-6 items-center">
+					<input
+						type="checkbox"
+						bind:checked={monochrome}
+						class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+					/>
+				</div>
+				<div class="ml-3 text-sm leading-6">
+					<label for="comments" class="font-medium text-gray-900">Monochrome</label>
+					<p id="comments-description" class="text-gray-500">
+						If the dither should be monochrome, or in color
+					</p>
+				</div>
 			</div>
-			<div class="ml-3 text-sm leading-6">
-				<label for="comments" class="font-medium text-gray-900">Monochrome</label>
-				<p id="comments-description" class="text-gray-500">
-					If the dither should be monochrome, or in color
-				</p>
-			</div>
-		</div>
 
-		{#if monochrome}
-			<fieldset>
-				<input type="color" name="color-light" bind:value={colorLight} />
-				<input type="color" name="color-dark" bind:value={colorDark} />
-			</fieldset>
-		{/if}
+			{#if monochrome}
+				<fieldset>
+					<input type="color" name="color-light" bind:value={colorLight} />
+					<input type="color" name="color-dark" bind:value={colorDark} />
+				</fieldset>
+			{/if}
 
-		<ImageSizeInput bind:width bind:aspectRatio />
+			<ImageSizeInput bind:width bind:aspectRatio />
+		</section>
+		<footer class="border-t border-gray-300 py-4 px-4">
+			<Button on:click={save} disabled={!loaded_image}>
+				<Icon src={ArrowDownTray} class="w-4 h-4" />
+				Save
+			</Button>
+		</footer>
+	</aside>
 
-		<button
-			type="button"
-			class="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 flex items-center justify-center gap-2"
-			on:click={save}
-		>
-			<Icon src={ArrowDownTray} class="w-4 h-4" />
-			Save
-		</button>
-	</section>
-	<section
-		class="bg-gray-300 flex-1 overflow-hidden touch-manipulation select-none"
-	>
+	<!--Main content-->
+	<section class="bg-gray-300 flex-1 overflow-hidden touch-manipulation select-none">
 		{#if loaded_image}
 			<div use:panzoomAction class="w-full h-full grid place-items-center">
 				<canvas
-					class:pixelated={css_image_rendering_pixelated}
+					class="pixelated"
 					use:dithering={{
 						image: loaded_image,
 						threshold,
---
+++ src/routes/(experiments)/dithering/Button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let disabled = false;
+
+    const appearance = {
+        normal: "bg-indigo-600 text-white shadow-sm hover:bg-indigo-500",
+        disabled: "bg-gray-200 text-gray-400 shadow-none cursor-not-allowed",
+    } as const;
+</script>
+
+<button
+    {disabled}
+	type="button"
+	class="{appearance[disabled ? "disabled" : "normal"]} rounded-md w-full px-2.5 py-1.5 text-sm font-semibold  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 flex items-center justify-center gap-2"
+	on:click
+>
+	<slot />
+</button>
