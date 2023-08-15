35fcf3a334f9e260da6cc477672e1797775c342a
Loris Sigrist <sigrist@hasch.io>
2023-08-15 12:04:01 +0200

    Hopefully fix Worker

--- src/routes/(experiments)/dithering/thresholdMapGeneration/main.ts
+++ src/routes/(experiments)/dithering/thresholdMapGeneration/main.ts
@@ -1,13 +1,13 @@
 import type { ThresholdMapOptions } from "./generation";
 import type { ThresholdMapGenerationWorker } from "./worker";
 import * as Comlink from "comlink";
-import worker_url from "./worker?url";
+import ThresholdWorker from "./worker?worker";
 
 let ThresholdMapWorker: Comlink.Remote<typeof ThresholdMapGenerationWorker> | null = null;
 
 export async function generateThresholdMap(options: ThresholdMapOptions): Promise<ImageData> {
     if (!ThresholdMapWorker) {
-        ThresholdMapWorker = Comlink.wrap(new Worker(worker_url, { type: "module"}));
+        ThresholdMapWorker = Comlink.wrap(new ThresholdWorker());
     }
 
     return await ThresholdMapWorker.generate(options);
