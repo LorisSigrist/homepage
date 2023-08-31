22c28b836981f495932f25e7e3e7f256d2368a6a
Loris Sigrist <sigrist@hasch.io>
2023-08-31 13:00:23 +0200

    Added Obra Dinn & Gameboy Presets

--- src/routes/(experiments)/dithering/palette/PaletteOptions.svelte
+++ src/routes/(experiments)/dithering/palette/PaletteOptions.svelte
@@ -36,13 +36,46 @@
 		{
 			name: 'Grayscale',
 			palette: ['#000000', '#333333', '#666666', '#999999', '#cccccc', '#ffffff'].map(hexToRGB)
-		},{
+		},
+		{
 			name: 'Red',
 			palette: ['#000000', '#F46842', '#AA2F0D', '#ffffff'].map(hexToRGB)
 		},
 		{
 			name: 'RGBY',
 			palette: ['#ff0000', '#00ff00', '#0000ff', '#ffff00'].map(hexToRGB)
+		},
+		{
+			name: 'Black, White, Red',
+			palette: ['#000000', '#ffffff', '#ff0000'].map(hexToRGB)
+		},
+		{
+			name: 'Black, White, Yellow',
+			palette: ['#000000', '#ffffff', '#ffff00'].map(hexToRGB)
+		},
+		{
+			name: 'Macintosh',
+			palette: ['#33321A', '#E5FEFF'].map(hexToRGB)
+		},
+		{
+			name: 'IBM 5151',
+			palette: ['#00EB61', '#25332F'].map(hexToRGB)
+		},
+		{
+			name: 'Zenith ZVM 1240',
+			palette: ['#40291e', '#fdca55'].map(hexToRGB)
+		},
+		{
+			name: 'Commodore 1084',
+			palette: ['#423091', '#88d7de'].map(hexToRGB)
+		},
+		{
+			name: 'IBM 8503',
+			palette: ['#25332F', '#EBE5CE'].map(hexToRGB)
+		},
+		{
+			name: 'Game Boy DMG-01',
+			palette: ['#CADC9F', '#0F380F', '#306230', '#8BAC0F', '#9BBC0F'].map(hexToRGB)
 		}
 	];
 
@@ -85,39 +118,41 @@
 	}
 </script>
 
-<Checkbox
-	bind:checked={generate_palette}
-	bind:input={generate_palette_input}
-	on:input={potentiallyReExtractColors}
-	label="Extract Colors from Image"
-/>
+<fieldset class="grid gap-4">
+	<Checkbox
+		bind:checked={generate_palette}
+		bind:input={generate_palette_input}
+		on:input={potentiallyReExtractColors}
+		label="Extract Colors from Image"
+	/>
 
-{#if generate_palette}
-	<label class="flex justify-between items-center">
-		<span>Number of Colors ({clamp(num_colors, MIN_EXTRACTED_COLORS, MAX_EXTRACTED_COLORS)})</span>
-		<input
-			type="number"
-			bind:value={num_colors}
-			on:blur={(e) => {
-				num_colors = clamp(num_colors, MIN_EXTRACTED_COLORS, MAX_EXTRACTED_COLORS);
-				console.log(num_colors);
-			}}
-			min={MIN_EXTRACTED_COLORS}
-			max={MAX_EXTRACTED_COLORS}
-			on:input={potentiallyReExtractColors}
-			class="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-		/>
-	</label>
-{:else}
-	<select
-		on:input={onPresetInput}
-		class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-	>
-		<option value={-1} selected disabled> Choose a Preset </option>
-		{#each presets as { name, palette: presetPalette }, i}
-			<option value={i}> {name} </option>
-		{/each}
-	</select>
-{/if}
+	{#if generate_palette}
+		<label class="flex justify-between items-center">
+			<span class="font-medium text-gray-900 text-sm">Number of Colors</span>
+			<input
+				type="number"
+				bind:value={num_colors}
+				on:blur={(e) => {
+					num_colors = clamp(num_colors, MIN_EXTRACTED_COLORS, MAX_EXTRACTED_COLORS);
+					console.log(num_colors);
+				}}
+				min={MIN_EXTRACTED_COLORS}
+				max={MAX_EXTRACTED_COLORS}
+				on:input={potentiallyReExtractColors}
+				class="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+			/>
+		</label>
+	{:else}
+		<select
+			on:input={onPresetInput}
+			class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+		>
+			<option value={-1} selected disabled> Choose a Preset </option>
+			{#each presets as { name, palette: presetPalette }, i}
+				<option value={i}> {name} </option>
+			{/each}
+		</select>
+	{/if}
 
-<PaletteInput bind:colors />
+	<PaletteInput bind:colors />
+</fieldset>
