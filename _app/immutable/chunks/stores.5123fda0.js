5c20db7755f5b1924a0230d2e0965c4e4c92fee4
LorisSigrist <LorisSigrist@users.noreply.github.com>
2023-09-02 00:08:59 +0000

    Deploying to gh-pages from @ LorisSigrist/homepage@1401f89a36f2a15f7d1e0d9f829313a7e1fc6f02 🚀

---
+++ _app/immutable/chunks/stores.5123fda0.js
@@ -0,0 +1,1 @@
+import"./index.ac0a233c.js";import{s as e}from"./singletons.151f5adf.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
