4711e3e11f6ea0fb3a65b50d7468e08677fff744
Loris Sigrist <sigrist@hasch.io>
2023-09-06 09:46:43 +0200

    Add MSPaint Vista color palette

--- src/routes/(experiments)/dithering/palette/PaletteOptions.svelte
+++ src/routes/(experiments)/dithering/palette/PaletteOptions.svelte
@@ -13,7 +13,7 @@
 	export let image: ImageData | null = null;
 	export let palette: ImageData | null = null;
 
-	export let colors: RGB[] = ['#000000', '#ffffff'].map(hexToRGB)
+	export let colors: RGB[] = ['#000000', '#ffffff'].map(hexToRGB);
 
 	const presets = [
 		{
@@ -76,6 +76,39 @@
 		{
 			name: 'Game Boy DMG-01',
 			palette: ['#CADC9F', '#0F380F', '#306230', '#8BAC0F', '#9BBC0F'].map(hexToRGB)
+		},
+		{
+			name: 'MS Paint Vista',
+			palette: [
+				'#000000',
+				'#464646',
+				'#787878',
+				'#b4b4b4',
+				'#dcdcdc',
+				'#ffffff',
+				'#990030',
+				'#9c5a3c',
+				'#ed1c24',
+				'#ffa3b1',
+				'#ff7e00',
+				'#e5aa7a',
+				'#ffc20e',
+				'#f5e49c',
+				'#fff200',
+				'#fff9bd',
+				'#a8e61d',
+				'#d3f9bc',
+				'#22b14c',
+				'#9dbb61',
+				'#00b7ef',
+				'#99d9ea',
+				'#4d6df3',
+				'#709ad1',
+				'#2f3699',
+				'#546d8e',
+				'#6f3198',
+				'#b5a5d5'
+			].map(hexToRGB)
 		}
 	];
 
