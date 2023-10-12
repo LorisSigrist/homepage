d78a3c464be54a32722f90e11a0fa7b7aa091186
Loris Sigrist <sigrist@hasch.io>
2023-10-12 22:22:13 +0200

    declarative exception handling

---
+++ src/routes/(site)/(articles)/declarative-exception-handling/+page.server.js
@@ -0,0 +1,9 @@
+/** @returns {import("$lib/articles/types").ArticleMetadata} */
+export function load() {
+    return {
+        title: "Declarative Exception Handling in JavaScript",
+        description: "Handling many different exception types in JavaScript is a pain. In this article we will play around with a declarative approach to exception handling.",
+        published: new Date("2023-10-12"),
+        author: "Loris Sigrist",
+    }
+}
