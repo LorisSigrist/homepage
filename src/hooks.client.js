2c84cef49ea90532c884eb5992ee9d9b23d9710e
Loris Sigrist <sigrist@hasch.io>
2023-12-24 14:12:07 +0100

    correct spelling

--- src/hooks.client.js
+++ src/hooks.client.js
@@ -4,9 +4,6 @@ posthog.init('phc_QjBHlSHQPP3PQAqq5Mb0VvvEp8qpR3ujkS2gUUla1TB', {
     api_host: 'https://us.posthog.com'
 });
 
-/**
- * @type {import("@sveltejs/kit").HandleClientError} param0 
- */
 export const handleError = ({ error, event }) => {
     posthog.capture('client error', { error, event });
 }
