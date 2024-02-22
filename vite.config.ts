40b5d8cccf48244b5ba116f394d85b4e995fecb3
Loris Sigrist <loris@sigrist.dev>
2024-02-22 18:14:39 +0100

    Install enhanced images

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,12 @@ import { colorThief } from './plugins/color-thief';
 import { imageSize } from './plugins/image-size';
 import { imagetools } from 'vite-imagetools'
 import { blurhash } from './plugins/blurhash';
+import { enhancedImages } from '@sveltejs/enhanced-img';
+
 
 
 export default defineConfig({
-	plugins: [blurhash, base64Loader, colorThief, imageSize, imagetools(), sveltekit()],
+	plugins: [blurhash, base64Loader, colorThief, imageSize, imagetools(), enhancedImages(), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
