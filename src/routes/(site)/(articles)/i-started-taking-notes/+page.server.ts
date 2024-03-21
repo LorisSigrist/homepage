66bbd1ec961ed4e9874c16ebf64b98f8fcf38c9e
Loris Sigrist <loris@sigrist.dev>
2024-03-21 22:02:45 +0100

    add note taking article

---
+++ src/routes/(site)/(articles)/i-started-taking-notes/+page.server.ts
@@ -0,0 +1,10 @@
+import type { ArticleMetadata } from "$lib/articles/types";
+
+export function load(): ArticleMetadata {
+    return {
+        title: "I've Started Taking Notes",
+        description: "There is a sea of content out there to consume. Only some of it is worth your time. Taking notes has helped me choose better & be more satisfied with my time.",
+        published: new Date("2024-03-21"),
+        author: "Loris Sigrist",
+    }
+}
