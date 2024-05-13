575aabd7fc91374ab367912f16dd53e610753653
Loris Sigrist <loris@sigrist.dev>
2024-05-13 20:29:06 +0200

    Unpublish notes post

--- src/routes/(site)/(articles)/i-started-taking-notes/+page.server.ts
+++ src/routes/(site)/(articles)/i-started-taking-notes/+page.server.ts
@@ -5,6 +5,7 @@ export function load(): ArticleMetadata {
         title: "I've Started Taking Notes",
         description: "There is a sea of content out there to consume. Only some of it is worth your time. Taking notes has helped me choose better & be more satisfied with my time.",
         published: new Date("2024-03-21"),
+        draft: true,
         author: "Loris Sigrist",
     }
 }
