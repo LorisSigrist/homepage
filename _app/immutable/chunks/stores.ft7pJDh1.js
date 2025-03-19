5da639df7ea21f5f06124c05a5113136e15736e7
LorisSigrist <LorisSigrist@users.noreply.github.com>
2025-03-19 00:13:15 +0000

    Deploying to gh-pages from @ LorisSigrist/homepage@b34f8488b2c8dd441be250b325bef46e74973f18 🚀

--- _app/immutable/chunks/stores.ft7pJDh1.js
+++ _app/immutable/chunks/stores.ft7pJDh1.js
@@ -1,4 +1,4 @@
-import { s as stores } from "./entry.BNCSltoD.js";
+import { s as stores } from "./entry.C-OIW8GD.js";
 const getStores = () => {
   const stores$1 = stores;
   return {
---
+++ _app/immutable/entry/start.B7NIdTfB.js
@@ -0,0 +1,4 @@
+import { a } from "../chunks/entry.C-OIW8GD.js";
+export {
+  a as start
+};
