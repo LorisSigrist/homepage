06d536eb74b22c88c7e2c7732f4e6ff6d5176aa4
LorisSigrist <LorisSigrist@users.noreply.github.com>
2025-12-06 00:14:11 +0000

    Deploying to gh-pages from @ LorisSigrist/homepage@90c37f062021915fe6f856def400020860533243 🚀

--- _app/immutable/chunks/entry.cYuOhdTx.js
+++ _app/immutable/chunks/entry.cYuOhdTx.js
@@ -310,9 +310,9 @@ function set(key, value, stringify = JSON.stringify) {
   } catch {
   }
 }
-const base = ((_a = globalThis.__sveltekit_ygbahd) == null ? void 0 : _a.base) ?? "";
-const assets = ((_b = globalThis.__sveltekit_ygbahd) == null ? void 0 : _b.assets) ?? base;
-const version = "1764893580935";
+const base = ((_a = globalThis.__sveltekit_9k4tv9) == null ? void 0 : _a.base) ?? "";
+const assets = ((_b = globalThis.__sveltekit_9k4tv9) == null ? void 0 : _b.assets) ?? base;
+const version = "1764979946585";
 const SNAPSHOT_KEY = "sveltekit:snapshot";
 const SCROLL_KEY = "sveltekit:scroll";
 const STATES_KEY = "sveltekit:states";
--- _app/immutable/entry/start.W3jvhQlz.js
+++
@@ -1,4 +0,0 @@
-import { a } from "../chunks/entry.Bfro01Qa.js";
-export {
-  a as start
-};
