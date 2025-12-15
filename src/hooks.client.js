1933d01b331bb8c067784b84417044e1857eb90f
Loris Sigrist <loris@sigrist.dev>
2025-12-15 10:28:52 +0100

    Remove posthog & add vite-flags for Node 25

--- src/hooks.client.js
+++
@@ -1,9 +0,0 @@
-import posthog from 'posthog-js';
-
-posthog.init('phc_QjBHlSHQPP3PQAqq5Mb0VvvEp8qpR3ujkS2gUUla1TB', {
-	api_host: 'https://us.posthog.com'
-});
-
-export const handleError = ({ error, event }) => {
-	posthog.capture('client error', { error, event });
-};
