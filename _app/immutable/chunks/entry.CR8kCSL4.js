9508bcae5cc6b4a592b55dd86e84c74cfc1ce69c
LorisSigrist <LorisSigrist@users.noreply.github.com>
2026-02-17 00:20:25 +0000

    Deploying to gh-pages from @ LorisSigrist/homepage@265db3561e4645c3ce2caed77000f4cffbbfe8f2 🚀

--- _app/immutable/chunks/entry.CR8kCSL4.js
+++ _app/immutable/chunks/entry.CR8kCSL4.js
@@ -301,9 +301,9 @@ function set(key, value, stringify = JSON.stringify) {
   } catch {
   }
 }
-const base = ((_a = globalThis.__sveltekit_5d5ba5) == null ? void 0 : _a.base) ?? "";
-const assets = ((_b = globalThis.__sveltekit_5d5ba5) == null ? void 0 : _b.assets) ?? base;
-const version = "1771201061837";
+const base = ((_a = globalThis.__sveltekit_7n44r9) == null ? void 0 : _a.base) ?? "";
+const assets = ((_b = globalThis.__sveltekit_7n44r9) == null ? void 0 : _b.assets) ?? base;
+const version = "1771287515862";
 const SNAPSHOT_KEY = "sveltekit:snapshot";
 const SCROLL_KEY = "sveltekit:scroll";
 const STATES_KEY = "sveltekit:states";
